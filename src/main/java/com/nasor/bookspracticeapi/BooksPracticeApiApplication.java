cd454c7cd5a19b4d901616c23419e64d042b6800
Nasor2 <penaortegasamuel@gmail.com>
2025-07-15 00:14:19 -0500

    Initial Commit

---
+++ src/main/java/com/nasor/bookspracticeapi/BooksPracticeApiApplication.java
@@ -0,0 +1,13 @@
+package com.nasor.bookspracticeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksPracticeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooksPracticeApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nasor/bookspracticeapi/BooksPracticeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nasor.bookspracticeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksPracticeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
