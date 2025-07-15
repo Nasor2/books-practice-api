00d7684bdde272f237f36331f4a98663cac35439
Nasor2 <penaortegasamuel@gmail.com>
2025-07-15 14:59:14 -0500

    feat: open api swagger implementation for document api

--- src/main/java/com/nasor/bookspracticeapi/controller/BookController.java
+++ src/main/java/com/nasor/bookspracticeapi/controller/BookController.java
@@ -2,6 +2,13 @@ package com.nasor.bookspracticeapi.controller;
 
 import com.nasor.bookspracticeapi.model.Book;
 import com.nasor.bookspracticeapi.repository.BookRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,19 +22,39 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/books")
 @Validated
+@Tag(name = "Book", description = "Book management operations in API")
 public class BookController {
 
     @Autowired
     private BookRepository bookRepository;
 
     // GET all books
+    @Operation(summary = "Get all books",
+            description = "Returns a list of all books.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Book.class))) // Returns list of books
+    })
     @GetMapping
     public ResponseEntity<List<Book>> getAllBooks() {
         return  ResponseEntity.ok(bookRepository.findAll());
     }
 
+    @Operation(summary = "Get Book by ID",
+            description = "Returns an specified book by its ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Book.class))),
+            @ApiResponse(responseCode = "404",
+                    content = @Content)
+    })
     @GetMapping("/{id}")
-    public ResponseEntity<Book> getBookById(@PathVariable Long id) {
+    public ResponseEntity<Book> getBookById(
+            @Parameter(description = "Book id")
+            @PathVariable Long id
+    ) {
         // find the book
         var book = bookRepository.findById(id);
 
@@ -39,6 +66,15 @@ public class BookController {
         return ResponseEntity.ok(book.get());
     }
 
+    @Operation(summary = "Create Book",
+            description = "Add a new book, ID will be generated automatically")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Book.class))),
+            @ApiResponse(responseCode = "400",
+                    content = @Content)
+    })
     @PostMapping
     public ResponseEntity<Book> createBook(@Valid @RequestBody Book book) {
         Book savedBook = bookRepository.save(book);
@@ -52,8 +88,23 @@ public class BookController {
         return ResponseEntity.created(location).body(savedBook);
     }
 
+    @Operation(summary = "Update Book",
+            description = "Update book information by its ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Book.class))),
+            @ApiResponse(responseCode = "404",
+                    content = @Content),
+            @ApiResponse(responseCode = "400",
+                    content = @Content)
+    })
     @PutMapping("/{id}")
-    public ResponseEntity<Book> updateBook(@PathVariable Long id, @Valid @RequestBody Book updateBook) {
+    public ResponseEntity<Book> updateBook(
+            @Parameter(description = "Book id")
+            @PathVariable Long id,
+            @Valid @RequestBody Book updateBook
+    ) {
         var existentBook = bookRepository.findById(id); // find book
 
         if (existentBook.isEmpty()){
@@ -73,8 +124,19 @@ public class BookController {
         return ResponseEntity.ok(savedBook);
     }
 
+    @Operation(summary = "Delete book",
+            description = "Delete an existent book by its ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204",
+                    content = @Content),
+            @ApiResponse(responseCode = "404",
+                    content = @Content)
+    })
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteBook(
+            @Parameter(description = "Book id")
+            @PathVariable Long id
+    ) {
         if (!bookRepository.existsById(id)) {
             return ResponseEntity.notFound().build(); // returns NOT_FOUND status if not found instance
         }
--- src/main/java/com/nasor/bookspracticeapi/model/Book.java
+++ src/main/java/com/nasor/bookspracticeapi/model/Book.java
@@ -1,5 +1,6 @@
 package com.nasor.bookspracticeapi.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,23 +15,30 @@ import lombok.*;
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "Book details")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "Book unique identifier (Auto generated)", example = "1", accessMode = Schema.AccessMode.READ_ONLY)
     private Long id;
 
-    @NotBlank(message = "Title canoot be blank")
+    @NotBlank(message = "Title cannot be blank")
+    @Schema(description = "Book title", example = "Cien años de soledad")
     private String title;
 
-    @NotBlank(message = "Author canoot be blank")
+    @NotBlank(message = "Author cannot be blank")
+    @Schema(description = "Book author", example = "Gabriel Garcia Marquez")
     private String author;
 
-    @NotBlank(message = "Description canoot be blank")
+    @NotBlank(message = "Description cannot be blank")
+    @Schema(description = "Book description", example = "Una novela fundamental del realismo mágico latinoamericano.")
     private String description;
 
     @NotBlank(message = "Category cannot be blank")
+    @Schema(description = "Book category", example = "Ficción, Realismo Mágico")
     private String category;
 
     @Min(value = 1, message = "Publication year must be greater than zero")
+    @Schema(description = "Book publication year", example = "1967")
     private int publicationDate;
 }
